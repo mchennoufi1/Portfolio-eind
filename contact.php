70f3b8483b7f0fef8366dff940279fc723a90387
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2021-10-18 17:23:36 +0200

    nice

--- contact.php
+++ contact.php
@@ -23,14 +23,47 @@
                             <h3 class="card-title">Contactgegevens</h3>
                             <p class="card-header">Naam: Mohammed Chennoufi</p>
                             <p class="card-header">Telefoonnummer:<a href="tel:0617631457">0617631457</a></p>
-                            <p class="card-header">E-Mail:
-                            <a href="mailto:mohammed_chennoufi@outlook.com">mohammed_chennoufi@outlook.com</a>
-                            </p>
+                            <p class="card-header">E-Mail: mohammed_chennoufi@outlook.com</p>
+
+                            <form action="" method="post">
+                            <label>Email Address</label>
+                            <input type="text" name="email_address" size="20">
+                            <br>
+                            <label>Your Feedback</label>
+                            <textarea name="feedback" cols="25" rows="5"></textarea>
+                            <br>
+                            <input type="submit" name="submit" value="submit">
+                            </form>
+
                             <p class="card-header">Social media: 
                                 <a href="https://www.instagram.com/mohammed070._/">mohammed070._</a>
                             </p>
                         </div>
                     </div>
+
+                    <?php
+                        if(isset($_POST['submit']))
+                        {
+                            $email_address = $_POST['email_address'];
+                            $feedback = $_POST['feedback'];
+                                
+                            $to = "mohammed_chennoufi@outlook.com";
+                            $subject = "Test mail";
+                            $headers = "From: nabil_chennoufi@outlook.com";
+
+                            $sent = mail($to,$subject,$feedback,$headers);
+
+                            if($sent)
+                            {
+                                echo "heeeeelgoed";
+                            }
+                            else
+                            {
+                                echo "niettttgoed";
+                            }
+                        }
+                    ?>
+
                 </div>
             </div>
             <br><br>
